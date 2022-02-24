524a330759d0222ecbd574697ec381b0feb6d969
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-02-24 10:29:14 +0530

    update todo example

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,6 +1,12 @@
-import { App } from "mahal";
-import Root from "./components/root.mahal";
+import { Mahal } from "mahal";
+import App from "@/app.mahal";
+import { registerGlobalFormatter } from "@/formatters";
+import config from "~/config";
 
-import "flexboot";
 
-new App(Root, '#app').create();
+const app = new Mahal(App, '#app');
+// register global formatter
+registerGlobalFormatter(app);
+// set config to be available globally
+app.global.config = config;
+app.create();
