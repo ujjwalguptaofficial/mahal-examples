34402b175aff9a81a889095c2d6ec8766cf395c4
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-27 12:05:53 +0530

    update count example with latest version of mahal

--- count/src/formatters/index.ts
+++
@@ -1,6 +0,0 @@
-import { Mahal } from "mahal";
-import { imgPath } from "./img_path";
-
-export function registerGlobalFormatter(app: Mahal) {
-    app.extend.formatter("imgPath", imgPath);
-}
--- count/src/index.ts
+++ count/src/index.ts
@@ -1,12 +1,11 @@
 import { Mahal } from "mahal";
 import App from "@/app.mahal";
-import { registerGlobalFormatter } from "@/formatters";
-import config from "~/config";
+import config from "~/config/env";
 
 
 const app = new Mahal(App, '#app');
-// register global formatter
-registerGlobalFormatter(app);
+ 
 // set config to be available globally
-app.global.config = config;
+app.global.env = config;
+
 app.create();
